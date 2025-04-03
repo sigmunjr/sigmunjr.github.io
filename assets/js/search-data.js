bb5c831653b84605ee6cfa080856f268171b5948
sigmunjr <sigmunjr@users.noreply.github.com>
2025-04-03 07:31:37 +0000

    Deploying to gh-pages from @ sigmunjr/sigmunjr.github.io@c680d4dcbabcdc298622868e726c8337cee26b70 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/index.html";
           },
         },{id: "post-selvkjørende-biler-fra-hype-til-virkelighet-snart-i-norge",
         
